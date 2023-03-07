b0773e1ae7a8a0384436273d76427120c977e38e
dvize <daniel.m.han@gmail.com>
2023-03-06 18:07:01 -0800

    Updated to Support Map Locations

--- Patch.cs
+++ Patch.cs
@@ -6,12 +6,10 @@ using EFT;
 using Comfort.Common;
 using UnityEngine;
 using NoNoZone;
-using EFT.AssetsManager;
-using System;
-using EFT.Bots;
-using EFT.InventoryLogic;
 using System.Collections.Generic;
 using System.Threading;
+using System;
+using EFT.UI.Matchmaker;
 
 namespace dvize.NoNoZone
 {
@@ -21,82 +19,89 @@ namespace dvize.NoNoZone
         {
             try
             {
-                var desiredType = PatchConstants.EftTypes.Single(x => x.Name == "BotsClass");
-                const BindingFlags flags = BindingFlags.Public | BindingFlags.Instance;
-                var desiredMethod = desiredType.GetMethod("AddFromList", flags);
-                return desiredMethod;
+                return typeof(BotsClass).GetMethod("AddFromList", BindingFlags.Instance | BindingFlags.Public);
             }
             catch
             {
-                Logger.LogError("NoNoZone: Failed to get target method");
+                Logger.LogInfo("NoNoZone: Failed to get target method");
             }
 
             return null;
         }
 
         [PatchPrefix]
-
         public static bool Prefix(BotsClass __instance, ref List<BotOwner> ___list_0)
         {
-            var player = Singleton<GameWorld>.Instance.MainPlayer;
-            var tempList = new List<BotOwner>(___list_0);
-            var playerPosition = player.Transform.position;
             var game = Singleton<GameWorld>.Instance;
+            var player = game.MainPlayer;
+            var playerPosition = player.Transform.position;
+            float distanceClearValue = 0f;
+            string location;
 
-            if (___list_0.Count > 0)
+
+            location = player.Location;
+
+
+            switch (location)
             {
-                foreach (var botOwner in tempList)
+                case "factory4_day":
+                    distanceClearValue = NoNoZonePlugin.factoryDistance.Value;
+                    break;
+                case "factory4_night":
+                    distanceClearValue = NoNoZonePlugin.factoryDistance.Value;
+                    break;
+                case "bigmap":
+                    distanceClearValue = NoNoZonePlugin.customsDistance.Value;
+                    break;
+                case "interchange":
+                    distanceClearValue = NoNoZonePlugin.interchangeDistance.Value;
+                    break;
+                case "reservbase":
+                    distanceClearValue = NoNoZonePlugin.reserveDistance.Value;
+                    break;
+                case "laboratory":
+                    distanceClearValue = NoNoZonePlugin.laboratoryDistance.Value;
+                    break;
+                case "lighthouse":
+                    distanceClearValue = NoNoZonePlugin.lighthouseDistance.Value;
+                    break;
+                case "shoreline":
+                    distanceClearValue = NoNoZonePlugin.shorelineDistance.Value;
+                    break;
+                case "woods":
+                    distanceClearValue = NoNoZonePlugin.woodsDistance.Value;
+                    break;
+                case "tarkovstreets":
+                    distanceClearValue = NoNoZonePlugin.tarkovstreetsDistance.Value;
+                    break;
+                default:
+                    distanceClearValue = 20.0f;
+                    break;
+            }
+
+
+            for (int i = 0; i < ___list_0.Count; i++)
+            {
+                var botOwner = ___list_0[i];
+                var botPosition = botOwner.Transform.position;
+                var distance = Vector3.Distance(botPosition, playerPosition);
+
+                if (distance < distanceClearValue)
                 {
-                    var botPosition = botOwner.Transform.position;
-                    var distance = Vector3.Distance(botPosition, playerPosition);
-
-                    if (distance < NoNoZonePlugin.DistanceClearBotSpawn.Value)
+                    var tempplayer = game.RegisteredPlayers.FirstOrDefault(p => p == botOwner.GetPlayer);
+                    if (tempplayer != null)
                     {
-                        Logger.LogInfo($"NoNoZone: {botOwner.Profile.Info.Settings.Role} Spawned Too Close");
-                        
-                        try
-                        {
-                            //find botOwner.GetPlayer in game.RegisteredPlayers list
-                            foreach (var tempplayer in game.RegisteredPlayers)
-                            {
-                                //if botOwner.GetPlayer == game.RegisteredPlayers enabled = false
-                                if (tempplayer == botOwner.GetPlayer)
-                                {
-                                    
-                                    Logger.LogDebug("NoNoZone: Disabled Player: " + tempplayer.Profile.Nickname + ", " + tempplayer.Profile.Info.Settings.Role);
+                        Logger.LogDebug($"Location Value is: {location} and the distance Cap is ({distanceClearValue}(m)");
+                        Logger.LogDebug($"NoNoZone: Disabled Player: {tempplayer.Profile.Nickname}, {tempplayer.Profile.Info.Settings.Role} ({distance}m)");
 
-                                    game.UnregisterPlayer(botOwner.GetPlayer);
-
-                                    __instance.RemovePlayer(botOwner.GetPlayer);
-                                    __instance.Remove(botOwner);
-
-                                    //var theweaponid = tempplayer.LastEquippedWeaponOrKnifeItem.Id;
-
-
-                                    //tempplayer.gameObject.DestroyAllChildren(false);
-                                    //botOwner.gameObject.DestroyAllChildren(false);
-
-                                    //game.DestroyLoot(theweaponid);
-                                    break;
-                                }
-                                
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            Logger.LogError("NoNoZone: Failed to remove Bot Owner and Player: " + e);
-                        }
-                        
-                        
+                        game.UnregisterPlayer(botOwner.GetPlayer);
+                        __instance.RemovePlayer(botOwner.GetPlayer);
+                        __instance.Remove(botOwner);
                     }
                 }
-
-                return true;
             }
 
             return true;
         }
     }
-
-
 }
--- Plugin.cs
+++ Plugin.cs
@@ -15,19 +15,78 @@ using dvize.NoNoZone;
 namespace NoNoZone
 {
 
-    [BepInPlugin("com.dvize.NoNoZone", "dvize.NoNoZone", "1.0.0")]
+    [BepInPlugin("com.dvize.NoNoZone", "dvize.NoNoZone", "1.1.0")]
     class NoNoZonePlugin : BaseUnityPlugin
     {
-
-        public static ConfigEntry<float> DistanceClearBotSpawn;
+        //create configentry for each map.  factory, interchange, laboratory, lighthouse, reserve, shoreline, woods, customs, tarkovstreets
+        
+        public static ConfigEntry<float> factoryDistance;
+        public static ConfigEntry<float> interchangeDistance;
+        public static ConfigEntry<float> laboratoryDistance;
+        public static ConfigEntry<float> lighthouseDistance;
+        public static ConfigEntry<float> reserveDistance;
+        public static ConfigEntry<float> shorelineDistance;
+        public static ConfigEntry<float> woodsDistance;
+        public static ConfigEntry<float> customsDistance;
+        public static ConfigEntry<float> tarkovstreetsDistance;
+        
         private void Awake()
         {
-
-            DistanceClearBotSpawn = Config.Bind(
+            
+            factoryDistance = Config.Bind(
                 "Main Settings",
-                "DistanceClearBotSpawn",
+                "factory",
                 20.0f,
                 "Distance to Keep Clear of Bot Spawns");
+
+            customsDistance = Config.Bind(
+                "Main Settings",
+                "customs",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
+            interchangeDistance = Config.Bind(
+                "Main Settings",
+                "interchange",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
+            laboratoryDistance = Config.Bind(
+                "Main Settings",
+                "labs",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
+            lighthouseDistance = Config.Bind(
+                "Main Settings",
+                "lighthouse",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
+            reserveDistance = Config.Bind(
+                "Main Settings",
+                "reserve",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
+            shorelineDistance = Config.Bind(
+                "Main Settings",
+                "shoreline",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
+            woodsDistance = Config.Bind(
+                "Main Settings",
+                "woods",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
+            tarkovstreetsDistance = Config.Bind(
+                "Main Settings",
+                "streets",
+                20.0f,
+                "Distance to Keep Clear of Bot Spawns");
+
 
             new BotClassSpawnerPatch().Enable();
 
